ad49a7a6cba91bc619c871d23ab2539fa4699d46
Maxi Codes <96955458+Kaanbey12@users.noreply.github.com>
2022-12-02 19:28:41 +0300

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,8 @@
+@echo off
+title Lyrox
+
+:Reconnected
+
+node index.js
+
+goto Reconnected
---
+++ settings/ayarlar.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTA0MTY1ODQ5MjkzMjU4NzUyMA.Glsdnp.xtWVJHinnruakSJGIROxd6xLkqqCs1Rk72lQaU",  // bot token gir :)
+    PREFIX: process.env.PREFIX || ".", // bot prefix
+    SAHİP: process.env.SAHİP || "979432515351765122", //sahip id :()
+    SHARDLOG: process.env.SHARDLOG || "1041660412082532393", // SHARD Log
+    HATALOG: process.env.HATALOG || "1041660412082532393", // Hata Log
+    LOADCOMMANDS: process.env.LOADCOMMANDS || "1041660412082532393", // LoadCommands Log
+    BİLDİRİLOG: process.env.BİLDİRİLOG || "1041660412082532393", // Bildiri Log
+    BOTUNADI: process.env.BOTUNADI || "Venue", // Botun Adını Giriniz.
+
+    //Yukardaki Herhangi Birşeyi Doldurmazsanız Hata Alırsınız.
+
+}
